5fa8d96a22cfdd4d4ec2331ab2d6bfcd4c9db082
Jeff Gosling <jeffery.gosling@gmail.com>
2017-05-19 00:33:02 +0100

    - include jquery and bootstrap - fixed fav icon

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+
 //CSS
-require('./css/common.css')
-require('./css/fonts.css')
-require('./css/footer.css')
-require('./css/header.css')
-require('./css/images.css')
+require('font-awesome/css/font-awesome.css');
+require('bootstrap/dist/css/bootstrap.css');
+
+require('./css/common.css');
+require('./css/fonts.css');
+require('./css/footer.css');
+require('./css/header.css');
+require('./css/images.css');
 
 //IMAGES
-import 'file-loader?name=[name].[ext]!./assets/images/fav-icon.png'
+import 'file-loader?name=[name].[ext]!./assets/images/fav-icon.png';
 
 //JS
-require('./js/site.js')
+require('./js/site.js');
