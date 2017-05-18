3f847693ac7ad3f64496d8ca238217c6432bda6f
Jeff Gosling <jeffery.gosling@gmail.com>
2017-05-18 23:58:47 +0100

    - fixed include paths after moving things around

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 //CSS
-require('../css/common.css')
-require('../css/fonts.css')
-require('../css/footer.css')
-require('../css/header.css')
-require('../css/images.css')
+require('./css/common.css')
+require('./css/fonts.css')
+require('./css/footer.css')
+require('./css/header.css')
+require('./css/images.css')
 
 //IMAGES
 
